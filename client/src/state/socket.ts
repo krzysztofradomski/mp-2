544ade9a0e55ab819fed8da9caccf64d531755f6
Krzysztof Radomski <krzysztof.radomski@netigate.se>
2024-09-24 23:50:46 +0200

    fix env variable

--- client/src/state/socket.ts
+++ client/src/state/socket.ts
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
-const socketUrl = process.env.SOCKET_URL || "http://localhost:4000";
+const socketUrl = import.meta.env.VITE_SOCKET_URL || "http://localhost:4000";
 
 export const socket = io(socketUrl);
